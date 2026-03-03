585ab87688a6981bc35dc2041edb9a328a8b2e5d
Donatoconturla <donatoconturla100@gmail.com>
2026-03-03 01:19:08 -0300

    Add Next.js configuration with strict mode and optimizations

--- next.config.js
+++ next.config.js
@@ -1,1 +1,8 @@
-
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    optimizePackageImports: ["three", "@react-three/fiber", "@react-three/drei"]
+  }
+};
+module.exports = nextConfig;
