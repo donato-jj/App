97c0ee2753f5a23a46650a959ca0a6ced4966e4a
Donatoconturla <donatoconturla100@gmail.com>
2026-03-03 01:26:36 -0300

    Add functions to download text files and data URLs

---
+++ src/lib/exporters.js
@@ -0,0 +1,20 @@
+export function downloadTextFile(filename, text, mime = "application/json") {
+  const blob = new Blob([text], { type: mime });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = filename;
+  document.body.appendChild(a);
+  a.click();
+  a.remove();
+  URL.revokeObjectURL(url);
+}
+
+export function downloadDataUrl(filename, dataUrl) {
+  const a = document.createElement("a");
+  a.href = dataUrl;
+  a.download = filename;
+  document.body.appendChild(a);
+  a.click();
+  a.remove();
+}
